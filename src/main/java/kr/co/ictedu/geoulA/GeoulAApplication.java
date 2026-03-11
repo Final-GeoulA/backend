e43803b3037694752d91c9c3037155010f48a87c
jsw0227 <zhalrtjddn01@gmail.com>
2026-03-11 15:15:53 +0900

    백엔드 초기 설정

---
+++ src/main/java/kr/co/ictedu/geoulA/GeoulAApplication.java
@@ -0,0 +1,41 @@
+package kr.co.ictedu.geoulA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@PropertySource(value = {"classpath:/properties/config.properties"}, encoding = "UTF-8")
+public class GeoulAApplication extends SpringBootServletInitializer {
+
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(GeoulAApplication.class);
+	}
+	
+	public static void main(String[] args) {
+		//SpringContainer가 시작및 종료
+		SpringApplication.run(GeoulAApplication.class, args);
+	}
+	
+	@Bean
+	public WebMvcConfigurer crosConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				System.out.println("Cros Allow Origin 실행");
+				registry.addMapping("/**")
+				.allowedOrigins("http://localhost:3001","http://localhost:3000","http://192.168.0.27:3001","http://192.168.0.27:3000")
+				.allowedHeaders("*")
+				.allowCredentials(true)
+				.allowedMethods("*").maxAge(3600);
+			}
+		};
+	}
+}
