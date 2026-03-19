205c55ddcdbcf1a23dc374ddcfdd3cb2930e1712
unknown <lina0430@naver.com>
2026-03-19 17:58:10 +0900

    Feat#9: 병원/약국 검색 및 저장 기능 구현

--- src/main/java/kr/co/ictedu/geoulA/vo/HospitalVO.java
+++ src/main/java/kr/co/ictedu/geoulA/vo/HospitalVO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class HospitalVO {
 	
-    private int hospitalId;     // 내부 병원 PK
+    private int hospitalId;     // 병원 PK
     private String placeId;     // 카카오 장소 ID
     private String name;        // 병원 이름
     private String address;     // 병원 주소
