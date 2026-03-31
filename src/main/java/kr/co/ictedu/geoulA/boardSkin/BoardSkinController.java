21aa711e10969f6bd29b0b6872b34b3de44308f0
Jsw0227 <zhalrtjddn01@gmail.com>
2026-03-31 14:32:11 +0900

    feat#45 : 자동 배포 및 S3 업로드, 이미지 리사이징 기능 구현

--- src/main/java/kr/co/ictedu/geoulA/boardSkin/BoardSkinController.java
+++ src/main/java/kr/co/ictedu/geoulA/boardSkin/BoardSkinController.java
@@ -1,13 +1,11 @@
 package kr.co.ictedu.geoulA.boardSkin;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,8 +16,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-//import io.lettuce.core.dynamic.annotation.Param;
 import jakarta.servlet.http.HttpServletRequest;
+import kr.co.ictedu.geoulA.config.S3Service;
 import kr.co.ictedu.geoulA.vo.BoardSkinVO;
 import kr.co.ictedu.geoulA.vo.BoardSkinCommVO;
 import kr.co.ictedu.geoulA.vo.PageVO;
@@ -33,34 +31,26 @@ public class BoardSkinController {
     
 	@Autowired
 	private BoardSkinService boardService;
-	
+
 	@Autowired
 	private BoardSkinCommService boardCommService;
-	
-	@Value("${spring.servlet.multipart.location}")
-	private String filePath;
-	
+
+	@Autowired
+	private S3Service s3Service;
+
 	@PostMapping("/add")
 	public ResponseEntity<?> addBoard(BoardSkinVO vo, HttpServletRequest req) {
 		vo.setReip(req.getRemoteAddr());
 		MultipartFile mf = vo.getMfile();
-		String oriFn =mf.getOriginalFilename();
-		System.out.println("파일이름: "+oriFn);
-		
-		StringBuilder path = new StringBuilder();
-		path.append(filePath).append("\\");
-		path.append(oriFn);
-		System.out.println("FullPath: "+ path);
-		File f = new File(path.toString());		
 		try {
-			mf.transferTo(f);   			
-			vo.setImgn(oriFn);
+			String imageUrl = s3Service.upload(mf);
+			vo.setImgn(imageUrl);
 			boardService.add(vo);
-			return ResponseEntity.ok().body("업로드 성공!");	
-		}catch (IllegalStateException |IOException e) {
+			return ResponseEntity.ok().body("업로드 성공!");
+		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("업로드 실패");	
+		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("업로드 실패");
 	}
 	@GetMapping("/del")
 	public void DelBoard(@RequestParam("num") int num) {
