f972e07e71f2344b6b3e6976e17128efdc12ea35
Jsw0227 <zhalrtjddn01@gmail.com>
2026-04-02 15:04:40 +0900

    feat#56 : 회원 이미지 사진 넣는 기능 구현

---
+++ src/main/java/kr/co/ictedu/geoulA/vo/SkinImgVO.java
@@ -0,0 +1,10 @@
+package kr.co.ictedu.geoulA.vo;
+import lombok.Data;
+
+@Data
+public class SkinImgVO {
+    private Long userSkinImgId;
+    private Long userId;
+    private String img;
+    private String bdate;
+}
