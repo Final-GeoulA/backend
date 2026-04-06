d18a6a9368f456d1b35a0cb3697991e41d55257e
tester <ktg950228@naver.com>
2026-04-06 12:16:47 +0900

    0406

--- src/main/java/kr/co/ictedu/geoulA/GeoulAApplication.java
+++ src/main/java/kr/co/ictedu/geoulA/GeoulAApplication.java
@@ -10,19 +10,19 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@PropertySource(value = {"classpath:/properties/config.properties"}, encoding = "UTF-8")
+@PropertySource(value = { "classpath:/properties/config.properties" }, encoding = "UTF-8")
 public class GeoulAApplication extends SpringBootServletInitializer {
-	
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
 		return builder.sources(GeoulAApplication.class);
 	}
-	
+
 	public static void main(String[] args) {
-		//SpringContainer가 시작및 종료
+		// SpringContainer가 시작및 종료
 		SpringApplication.run(GeoulAApplication.class, args);
 	}
-	
+
 	@Bean
 	public WebMvcConfigurer crosConfigurer() {
 		return new WebMvcConfigurer() {
@@ -30,17 +30,7 @@ public class GeoulAApplication extends SpringBootServletInitializer {
 			public void addCorsMappings(CorsRegistry registry) {
 				System.out.println("Cros Allow Origin 실행");
 				registry.addMapping("/**")
-<<<<<<< Updated upstream
-<<<<<<< HEAD
-
-				.allowedOrigins("http://localhost:3001","http://localhost:3000","http://192.168.0.23:3001","http://192.168.0.23:3000")
-
-=======
-				.allowedOrigins("http://localhost:3001","http://localhost:3000","http://192.168.0.56:3001","http://192.168.0.56:3000")
->>>>>>> 3c2c07d36b62b82555444bc86be93a953e5ee61c
-=======
-				.allowedOrigins("http://localhost:3001","http://localhost:3000","http://192.168.0.156:3001","http://192.168.0.156:3000")
->>>>>>> Stashed changes
+				.allowedOrigins("http://localhost:3001","http://localhost:3000","http://192.168.0.25:3001","http://192.168.0.25:3000")
 				.allowedHeaders("*")
 				.allowCredentials(true)
 				.allowedMethods("*").maxAge(3600);
