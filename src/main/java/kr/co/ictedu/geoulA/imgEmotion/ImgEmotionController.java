325b4ad2ad9c61580c07e81d0b55398be6f71b1c
tester <ktg950228@naver.com>
2026-04-08 10:49:01 +0900

    0408

--- src/main/java/kr/co/ictedu/geoulA/imgEmotion/ImgEmotionController.java
+++ src/main/java/kr/co/ictedu/geoulA/imgEmotion/ImgEmotionController.java
@@ -1,13 +1,9 @@
 package kr.co.ictedu.geoulA.imgEmotion;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.servlet.http.HttpSession;
 import kr.co.ictedu.geoulA.vo.ImgEmotionVO;
-import kr.co.ictedu.geoulA.vo.SkinAnalysisDiseaseVO;
 import kr.co.ictedu.geoulA.vo.UsersVO;
 
 @RestController
